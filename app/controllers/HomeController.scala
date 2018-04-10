f46a8ece1dec794ec44f0fc69f365070a01affb5
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2018-04-10 21:20:50 +0530

    play project structure

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+   * Create an Action to render an HTML page with a welcome message.
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def postItem = Action.async(parse.json) { implicit request =>
+    Future(Ok(""))
+  }
+
+  def handleStream = Action.async { implicit request =>
+    Future(Ok(""))
+  }
+
+}

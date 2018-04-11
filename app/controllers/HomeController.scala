f2b48f9bf25b6e2db605a022766acb506dd57dd9
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2018-04-11 15:26:45 +0530

    binded for sse. bad luck not working

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,9 +3,25 @@ package controllers
 import javax.inject._
 
 import play.api.mvc._
+import akka.actor.{Actor, ActorRef, ActorSystem, PoisonPill, Props}
+import akka.stream.scaladsl.{Source, StreamConverters}
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.EventSource
+import play.api.libs.iteratee.{Concurrent, Iteratee}
+
+case class Start(out: Concurrent.Channel[String])
+case class NewItem(name: String)
+
+class Listener extends Actor {
+
+  var out: Option[Concurrent.Channel[String]] = None
+  def receive = {
+    case Start(out)    => print(out);this.out = Some(out)
+    case NewItem(name) => print(name);this.out.map(_.push(name))
+  }
+}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -13,6 +29,14 @@ import scala.concurrent.ExecutionContext.Implicits.global
  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  val system = ActorSystem("SSE")
+
+  val listener = system.actorOf(Props[Listener], "listener")
+
+  val (out, channel) = Concurrent.broadcast[String]
+
+  listener ! Start(channel)
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -25,11 +49,16 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   }
 
   def postItem = Action.async(parse.json) { implicit request =>
-    Future(Ok(""))
+    val postedItem = (request.body \ "name").validate[String].getOrElse("Undefined")
+    listener ! NewItem(postedItem)
+    Future(Ok(postedItem))
   }
 
-  def handleStream = Action.async { implicit request =>
-    Future(Ok(""))
+  def handleStream = Action { implicit request =>
+    val consume = Iteratee.consume[String]()
+    val resultFromEnumerator = Iteratee.flatten(out(consume)).run
+    val source = Source.fromFuture(resultFromEnumerator)
+    Ok.chunked(source via EventSource.flow).as("text/event-stream")
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -10,7 +10,14 @@ resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
       
 scalaVersion := "2.12.2"
 
-libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.11",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.11",
+  "com.typesafe.play" %% "play-iteratees" % "2.6.1",
+  "com.typesafe.play" %% "play-iteratees-reactive-streams" % "2.6.1",
+  ws ,
+  guice
+)
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
